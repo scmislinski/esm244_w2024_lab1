717d734fc0fadea65940eb9fc4d9d32c17415393
scmislinski <scmislinski@bren.ucsb.edu>
2024-01-11 16:23:16 -0800

    First Draft of Lab 1

---
+++ scripts/Untitled.R
@@ -0,0 +1,18 @@
+#Advanced Data Science Lab 1 Jan 11, 2024
+
+
+#New library for linking github to r
+library(usethis)
+
+#to get stuff from github. Go to new project and select "verson control"
+
+
+
+
+
+
+
+
+
+
+
